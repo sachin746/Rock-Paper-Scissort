76bd17e29fe080eae1638efa125c5cebd3a4eb47
Sachin746 <sachinbhadur9@gmail.com>
2022-10-24 17:26:22 +0530

    version 1 of rock paper scissors with only console and alert functions

---
+++ main.js
@@ -0,0 +1,68 @@
+function getComputerChoice(){
+    let x=Math.floor(Math.random()*3);
+    return arr[x];
+}
+
+let arr=["Rock","Paper","Scissors"]
+
+function playRound(playerChoice,computerChoice){
+
+    playerChoice=playerChoice.toLowerCase();
+    computerChoice=computerChoice.toLowerCase();
+
+    if(playerChoice==computerChoice){
+        return "Its a Draw Play Again";
+    }
+
+    if(playerChoice==="rock"){
+        if(computerChoice==="scissors"){
+            return "Yay You Won! Rock beats Scissors";
+        }else{
+            return "You Lose! Paper beats Rock";
+        }
+    }else if( playerChoice==="paper"){
+        if(computerChoice==="rock"){
+            return "Yay You Won! Paper beats Rock";
+        }else{
+            return "You Lose! Scissors beats Paper";
+        }
+    }else{
+        if(computerChoice==="paper"){
+            return "Yay You Won! Scissors beats Paper";
+        }else{
+            return "You Lose! Rock beats Scissors";
+        }
+    }
+}
+let computerScore=0,playerScore=0;
+function game(){
+
+    for(let i=0; i<5; i++){
+        let playerChoice=prompt("type your choice").toLowerCase();
+        while(playerChoice!="rock"&&playerChoice!="paper"&&playerChoice!="scissors"){
+            alert("choose between Rock Paper Scissors");
+            playerChoice=prompt("type your choice").toLowerCase();
+        }
+        computerChoice=getComputerChoice();
+        let x=playRound(playerChoice,computerChoice);
+        if(x.includes("Won"))playerScore++;
+        else if(x.includes("Draw")){
+            playerScore++;
+            computerScore++;
+        }else{
+            computerScore++;
+        }
+        console.log(x);
+        alert(x+ ' computerScore :'+computerScore+' playerScore :'+playerScore);
+    }
+}
+game();
+if(computerScore>playerScore){
+    console.log("computer won with a score of "+computerScore," against you of score of "+playerScore);
+}
+else if(computerScore<playerScore){
+    console.log("You won with a score of "+playerScore," against computer of score of "+computerScore);
+}else{
+     console.log("Its a Draw with your score and computer score of "+computerScore);
+
+}
